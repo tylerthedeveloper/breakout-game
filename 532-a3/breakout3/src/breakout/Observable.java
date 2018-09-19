54b517235a480954c8276b12ee3e709a218cc960
Tyler Citrin <tcitrin@indiana.edu>
2018-09-19 14:00:28 -0400

    t `Revert "p3"
    This reverts commit 89ce379da30529131aff7c0fcc4f6d2c557c345e.

--- 532-a3/breakout3/src/breakout/Observable.java
+++
@@ -1,9 +0,0 @@
-package breakout;
-
-import breakout.ScreenElements.Sprite;
-
-public interface Observable {
-	public void registerObserver(Sprite o);
-	public void removeObserver(Sprite o);
-	public void notifyObservers();
-}
--- 532-a3/breakout3/src/breakout/ScreenElements/Sprite.java
+++
@@ -1,8 +0,0 @@
-package breakout.ScreenElements;
-
-import java.awt.Graphics;
-
-public interface Sprite {
-	public void update();
-	public void draw(Graphics g);
-}
